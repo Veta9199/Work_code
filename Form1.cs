73c9c884168190ce57ac759fdbd22115eeff98f6
_Veta_ <heggelil5@gmail.com>
2025-06-17 21:10:47 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,51 +12,66 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WindowsFormsApp2
 {
-    public partial class лаб1 : Form
+    public partial class Form1 : Form
     {
-        public лаб1()
+        public Form1()
         {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string inputA = textBox1.Text.Trim();
+            string inputB = textBox2.Text.Trim();
+            string inputC = textBox3.Text.Trim();
+
+            lblResult.Text = ""; // очистка результата
+
+            // Проверка на пустые поля
+            if (string.IsNullOrEmpty(inputA) || string.IsNullOrEmpty(inputB) || string.IsNullOrEmpty(inputC))
+            {
+                lblResult.Text = "Ошибка! Все три поля должны быть заполнены.";
+                return;
+            }
+
             try
             {
-                int a = Convert.ToInt32(textBox1.Text);
-                int b = Convert.ToInt32(textBox2.Text);
-                int c = Convert.ToInt32(textBox3.Text);
+                int a = Convert.ToInt32(inputA);
+                int b = Convert.ToInt32(inputB);
+                int c = Convert.ToInt32(inputC);
 
+                // Проверка на положительность сторон
+                if (a <= 0 || b <= 0 || c <= 0)
+                {
+                    lblResult.Text = "Ошибка! Стороны треугольника должны быть положительными.";
+                    return;
+                }
 
+                // Проверка существования треугольника
                 if (a + b > c && a + c > b && b + c > a)
                 {
                     if (a == b && b == c)
                     {
-                        label2.Text = "Что-то получилось, а именно = полученный треугольник - равносторонний";
+                        lblResult.Text = "Треугольник равносторонний.";
                     }
                     else if (a == b || a == c || b == c)
                     {
-                        label2.Text = "Что-то получилось, а именно = полученный треугольник - равнобедренный";
+                        lblResult.Text = "Треугольник равнобедренный.";
                     }
                     else
                     {
-                        label2.Text = "Что-то получилось, а именно = полученный треугольник - разносторонний";
+                        lblResult.Text = "Треугольник разносторонний.";
                     }
                 }
                 else
                 {
-                    label2.Text = "Ошибка! Треугольник с такими сторонами не существует.";
+                    lblResult.Text = "Ошибка! Треугольник с такими сторонами не существует.";
                 }
             }
             catch (FormatException)
             {
-                label2.Text = "Ошибка! Введите корректные числовые значения.";
+                lblResult.Text = "Ошибка! Введите корректные целые числа.";
             }
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
